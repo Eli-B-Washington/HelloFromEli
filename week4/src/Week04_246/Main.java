556d041eb9b4de1ce752fe78b29bab63717ff0bd
ehandy <evan@handysoftware.com>
2020-10-08 13:28:05 -0400

    Added Evan's name

--- week4/src/Week04_246/Main.java
+++ week4/src/Week04_246/Main.java
@@ -3,6 +3,8 @@ package Week04_246;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello Eli, my name is Evan.");
 	// write your code here
     }
 }
